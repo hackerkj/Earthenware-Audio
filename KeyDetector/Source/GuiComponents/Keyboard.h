e25d2fb2ac949523706889cde6b7b1cf2e167680
welagejl1684 <43866259+welagejl1684@users.noreply.github.com>
2023-02-27 21:22:13 -0800

    assigning midi notes

--- KeyDetector/Source/GuiComponents/Keyboard.h
+++ KeyDetector/Source/GuiComponents/Keyboard.h
@@ -13,9 +13,10 @@ public:
         startTime (juce::Time::getMillisecondCounterHiRes() * 0.001)
     {
         setOpaque (true);
-
+        setKey(KeyFinder::key_t::C_MAJOR);
         addAndMakeVisible (keyboardComponent);
         keyboardState.addListener (this);
+        keyboardState.reset();
 
         setSize (800, 350);
     }
@@ -37,64 +38,87 @@ public:
         {
             return;
         }
-
         currentKey = key;
 
         switch(currentKey)
         {
             case KeyFinder::key_t::A_FLAT_MAJOR:
+                keyboardComponent.setCurrentKey({6, 8, 0, 1, 3, 5, 7});
                 break;
             case KeyFinder::key_t::A_FLAT_MINOR:
+                keyboardComponent.setCurrentKey({1, 3, 4, 6, 8, 10, 11});
                 break;
             case KeyFinder::key_t::A_MAJOR:
+                keyboardComponent.setCurrentKey({1, 2, 4, 6, 8, 9, 11});
                 break;
             case KeyFinder::key_t::A_MINOR:
+                keyboardComponent.setCurrentKey({0, 2, 4, 5, 7, 9, 11});
                 break;
             case KeyFinder::key_t::B_FLAT_MAJOR:
+                keyboardComponent.setCurrentKey({0, 2, 3, 5, 7, 9, 10});
                 break;
             case KeyFinder::key_t::B_FLAT_MINOR:
+                keyboardComponent.setCurrentKey({0, 1, 3, 5, 7, 8, 10});
                 break;
             case KeyFinder::key_t::B_MAJOR:
+                keyboardComponent.setCurrentKey({1, 3, 4, 6, 8, 10, 11});
                 break;
             case KeyFinder::key_t::B_MINOR:
+                keyboardComponent.setCurrentKey({1, 2, 4, 6, 8, 9, 11});
                 break;
             case KeyFinder::key_t::C_MAJOR:
+                keyboardComponent.setCurrentKey({0, 2, 4, 5, 7, 9, 11});
                 break;
             case KeyFinder::key_t::C_MINOR:
+                keyboardComponent.setCurrentKey({0, 2, 3, 5, 7, 9});
                 break;
             case KeyFinder::key_t::D_FLAT_MAJOR:
+                keyboardComponent.setCurrentKey({0, 1, 3, 5, 6, 8});
                 break;
             case KeyFinder::key_t::D_FLAT_MINOR:
+                keyboardComponent.setCurrentKey({1, 3, 4, 6, 8, 10, 11});
                 break;
             case KeyFinder::key_t::D_MAJOR:
+                keyboardComponent.setCurrentKey({1, 2, 4, 6, 7, 9, 11});
                 break;
             case KeyFinder::key_t::D_MINOR:
+                keyboardComponent.setCurrentKey({0, 2, 4, 5, 7, 9, 11});
                 break;
             case KeyFinder::key_t::E_FLAT_MAJOR:
+                keyboardComponent.setCurrentKey({0, 2, 3, 5, 7, 8, 10});
                 break;
             case KeyFinder::key_t::E_FLAT_MINOR:
+                keyboardComponent.setCurrentKey({1, 3, 5, 6, 8, 10, 11});
                 break;
             case KeyFinder::key_t::E_MAJOR:
+                keyboardComponent.setCurrentKey({1, 3, 4, 6, 8, 9, 11});
                 break;
             case KeyFinder::key_t::E_MINOR:
+                keyboardComponent.setCurrentKey({0, 2, 4, 6, 7, 9, 11});
                 break;
              case KeyFinder::key_t::F_MAJOR:
+                keyboardComponent.setCurrentKey({0, 2, 4, 5, 7, 9, 10});
                 break;
             case KeyFinder::key_t::F_MINOR:
+                keyboardComponent.setCurrentKey({0, 2, 3, 5, 7, 8, 10});
                 break;
             case KeyFinder::key_t::G_FLAT_MAJOR:
+                keyboardComponent.setCurrentKey({1, 3, 5, 6, 8, 10, 11});
                 break;
             case KeyFinder::key_t::G_FLAT_MINOR:
+                keyboardComponent.setCurrentKey({1, 3, 4, 6, 8, 9, 11});
                 break;
             case KeyFinder::key_t::G_MAJOR:
+                keyboardComponent.setCurrentKey({0, 2, 4, 6, 7, 9, 11});
                 break;
             case KeyFinder::key_t::G_MINOR:
+                keyboardComponent.setCurrentKey({0, 2, 4, 5, 7, 9, 10});
                 break;
             case KeyFinder::key_t::SILENCE:
+                keyboardComponent.setCurrentKey({});
                 break;
-
-
         }
+        keyboardComponent.timerCallback();
         repaint();
     }
 
@@ -113,7 +137,8 @@ private:
 
     juce::MidiKeyboardState keyboardState;            
     juce::MidiKeyboardComponent keyboardComponent;    
-    KeyFinder::key_t currentKey;
+    std::vector<int> midiNumberList;
+    KeyFinder::key_t currentKey = KeyFinder::key_t::C_MAJOR;
 
     double startTime;
     //==============================================================================
